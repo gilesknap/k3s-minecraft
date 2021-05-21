2208a6495ca71b23ceff9211db977b053cd22951
Giles Knap <gilesknap@gmail.com>
2021-05-21 22:02:53 +0100

    fully working prometheus node exporter and grafana

--- monitoring/deploy-monitoring.sh
+++ monitoring/deploy-monitoring.sh
@@ -7,9 +7,23 @@ kubectl config use-context monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 
+# for some reason nfs-storageclass causes grafana pod to exit 1 with no log
+# so use local-path and tie to my server gknuc for local storage
 helm upgrade -i grafana grafana/grafana \
-  --set persistence.storageClassName=nfs-storageclass
+  --set persistence.storageClassName=local-path \
+  --set persistence.enabled=true \
+  --set nodeSelector."kubernetes\\.io/hostname"=gknuc
 helm upgrade -i prometheus prometheus-community/prometheus \
-  --set alertmanager.persistentVolume.storageClassName=nfs-storageclass \
+  --set alertmanager.persistentVolume.storageClass=nfs-storageclass \
   --set server.persistentVolume.storageClass=nfs-storageclass \
-  --set pushgateway.persistentVolume.storageClass=nfs-storageclass
+  --set pushgateway.persistentVolume.storageClass=nfs-storageclass \
+  --set pushgateway.persistentVolume.enabled=true \
+  --set nodeSelector."kubernetes\\.io/hostname"=gknuc
+
+# to use grafana:
+# create new data source - prometheus with
+#  Url: http://localhost:9090
+#  Access: Browser
+# you also need the 9090 port-forward from gui-proxies.sh
+# then import a dashboard and choose id 1860 (node exporter full)
+# this gives extensive stats on all nodes in the cluster
